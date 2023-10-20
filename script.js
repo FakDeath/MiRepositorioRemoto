e7aa4e4f677a030dc6d0c8b880635a6af1c8bfdc
Facundo Peralta <facundop85@gmail.com>
2023-10-19 21:44:59 -0300

    Mi primer commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//alert("Hola este es mi Javascript");
+//let nombre = "Facundo";
+//console.log (nombre)
+
+
+
+//Condicionales
+
+consta
+
+let menu_responsive = document.querySelector(".checkbtn");
+      menu_responsive.onclick = function () {
+        ulBar = document.querySelector(".ulbar");
+        ulBar.classList.toggle("active");
+      };
+
+function suma(a, b){
+  return a+b;
+}
+
+console.log (suma (3, 7));
+
+
+//console.log(elemento);
